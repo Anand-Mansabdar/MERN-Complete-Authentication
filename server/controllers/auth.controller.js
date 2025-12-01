7e892b4e5ac9efc5bf216642624442fe0b03abd4
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-12-02 00:34:57 +0530

    Basic Server Setup and User Registration

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,45 @@
+import userModel from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Missing Name or email or password",
+    });
+  }
+
+  try {
+    const userExists = await userModel.findOne({ email });
+    if (userExists) {
+      return res.status(401).json({
+        success: false,
+        message: `User already exists`,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Missing Name or email or password",
+    });
+  }
+};
