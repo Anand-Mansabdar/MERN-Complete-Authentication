f83f8f31e136d09efecf60b4a473ac7b39cef0e6
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-12-03 23:53:55 +0530

    Implemented userAuth Middleware and API endpoints

---
+++ server/middlewares/user.middleware.js
@@ -0,0 +1,34 @@
+import jwt, { decode } from "jsonwebtoken";
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(400).json({
+      success: false,
+      message: `Invalid token`,
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.id) {
+      req.body.userId = decoded.id;
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: `Not authorized`,
+      });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+export default userAuth;
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  register,
+  sendVerifyOtp,
+  verifyEmail,
+} from "../controllers/auth.controller.js";
+import userAuth from "../middlewares/user.middleware.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+
+authRouter.post("/verification-otp", userAuth, sendVerifyOtp);
+authRouter.post("/verify-account", userAuth, verifyEmail);
+
 
 export default authRouter;
