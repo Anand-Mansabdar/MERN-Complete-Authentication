98dd51337504336bdfa93b0c91299d15fd18c01a
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-12-06 21:45:51 +0530

    scaffold: implemented otp verification function

--- client/src/pages/Verification.jsx
+++ client/src/pages/Verification.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { toast } from "react-toastify";
@@ -6,10 +6,11 @@ import axios from "axios";
 import { AppContext } from "../context/AppContext";
 
 const Verification = () => {
+  axios.defaults.withCredentials = true;
   const navigate = useNavigate();
   const inputRefs = useRef([]);
 
-  const { backendUrl, setIsLoggedIn, getUserData, userData } =
+  const { backendUrl, setIsLoggedIn, getUserData, userData, isLoggedIn } =
     useContext(AppContext);
 
   const handleInput = (e, index) => {
@@ -34,7 +35,32 @@ const Verification = () => {
     });
   };
 
-  
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const otpArray = inputRefs.current.map((e) => e.value);
+      const otp = otpArray.join("");
+      console.log("Entered OTP:", otp);
+
+      const { data } = await axios.post(
+        `${backendUrl}/api/auth/verify-account`,
+        { otp }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getUserData();
+        navigate("/");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    isLoggedIn && userData && userData.isAccountVerified && navigate("/");
+  }, [isLoggedIn, userData]);
   return (
     <div className="flex justify-center items-center min-h-screen bg-linear-to-br from-blue-200 to-rose-200">
       <img
@@ -44,7 +70,10 @@ const Verification = () => {
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
 
-      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+      <form
+        onSubmit={onSubmitHandler}
+        className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm"
+      >
         <h1 className="text-white text-2xl font-semibold text-center mb-4">
           Verify Your Email
         </h1>
